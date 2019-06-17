5ba8f94c6149669edaf3ccd99271076f0c610af6
Kevin-Kip <masterfork5@gmail.com>
2019-06-17 15:32:15 +0300

    Finished UI

--- app/src/main/java/com/revosleap/whitescreen/MainActivity.kt
+++ app/src/main/java/com/revosleap/whitescreen/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.revosleap.whitescreen
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
